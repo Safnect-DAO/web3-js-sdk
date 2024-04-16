9c2aeb2ef682a04e866b317d29050737b1337629
shiw1962 <43884967+shiw1962@users.noreply.github.com>
2024-04-16 16:40:10 +0800

    Update wallet.js

--- wallet.js
+++ wallet.js
@@ -114,7 +114,7 @@ export async function getSpaceBalnace(network, address, callback) {
 }
 
 // 获取Space交易明细
-export async function getSpaceRecords(network, address, confirmed, callback) {
+export async function getSpaceRecords(network, address, callback, confirmed) {
   var url;
   var suffix = 'address/' + address + '/tx?confirmed=' + confirmed;
   if (network == 'testnet') {
